99cc8f60032fd368f1d254910e5cb86247f57697
许翠华 <xucuihua@xucuihuadeMacBook-Air.local>
2015-06-23 13:13:57 +0800

    Initial Commit

---
+++ JiaZheng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JiaZheng
+//
+//  Created by 许翠华 on 15/6/23.
+//  Copyright (c) 2015年 Tracey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JiaZheng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JiaZheng
+//
+//  Created by 许翠华 on 15/6/23.
+//  Copyright (c) 2015年 Tracey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
